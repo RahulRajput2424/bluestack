9bacd140a15aaa5d99bca8100657727757e46cef
rrajput <rahul.rajput@achieve3000.com>
2021-01-05 01:41:24 +0530

    Adding custom user model

--- bluestackApp/models.py
+++ bluestackApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+      (1, 'demo1'),
+      (2, 'demo2'),
+  )
+    user_type = models.IntegerField(choices=USER_TYPE_CHOICES,default=None)
+    username = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    mobileNumber = models.CharField(null=True,max_length=18, unique=True)
