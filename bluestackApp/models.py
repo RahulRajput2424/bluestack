2462a5ff2e12444395daae30875fa26ccd34e885
rrajput <rahul.rajput@achieve3000.com>
2021-01-07 14:25:12 +0530

    Add Usersign Up Functionality

--- bluestackApp/models.py
+++ bluestackApp/models.py
@@ -9,3 +9,4 @@ class User(AbstractUser):
     username = models.CharField(max_length=255,unique=True)
     email = models.EmailField(db_index=True, unique=True)
     mobileNumber = models.CharField(null=True,max_length=18, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
